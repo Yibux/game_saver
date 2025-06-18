1efcf84f8565afa34d1df13c201e61e7d1822b35
Yibux <240810@edu.p.lodz.pl>
2025-06-18 23:40:54 +0200

    new save

---
+++ save_game_pusher_win.ps1
@@ -0,0 +1,5 @@
+python game_unpack.py
+
+git add *
+git commit -m "new save"
+git push -u origin main
---
+++ upload_saves.ps1
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git pull
+
+# Uruchom skrypt Pythona (zmień nazwę pliku jeśli potrzeba)
+python game_unpack.py
